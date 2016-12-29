ac0bb59c0d9b0c61012df248dfe024bd10ef070e
LV <cryboyofyu@gmail.com>
2016-12-29 10:58:07 +0800

    Initial Commit

---
+++ XYBadgeExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XYBadgeExample
+//
+//  Created by XY Lv on 16/12/29.
+//  Copyright © 2016年 吕欣宇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XYBadgeExample/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XYBadgeExample
+//
+//  Created by XY Lv on 16/12/29.
+//  Copyright © 2016年 吕欣宇. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
